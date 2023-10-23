f0f45d827c0ebf828394e2a83aab6fc910037546
Spchdt <spchdt@gmail.com>
2023-10-23 18:07:25 +0700

    Initial Commit

---
+++ FireFocus/FireFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireFocusApp.swift
+//  FireFocus
+//
+//  Created by Supachod Trakansirorut on 23/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct FireFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
